66085d68fb2c1b7aaff03dbd7488d7bb6af9da92
pedrobosilva <pedrob_oliveira@hotmail.com>
2016-08-23 19:07:03 -0300

    Arrumando Projeto Para Mogi
    Criei Cena e coloquei um cubo que gira em "x"

---
+++ Assets/Scripts/SpintCube.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpintCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Rotate (5, 0, 0);
+	
+	}
+}
