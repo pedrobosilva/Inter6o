836bf54ada2ce5e5a5c847b158f4e9de179e190c
pedrobosilva <pedrob_oliveira@hotmail.com>
2016-10-12 13:38:26 -0300

    GameControl com Lista
    Game COntrol funcionando com listas porém com o save não funcional

---
+++ Assets/Scripts/ScriptsBanco/GameControl.cs
@@ -0,0 +1,205 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+public class GameControl : MonoBehaviour {
+
+
+	public static GameControl gControl;
+	public GameObject[] perguntasBdGo;
+	public List<PerguntasClass> perguntasList = new List<PerguntasClass> ();
+	public int pontos;
+	public int funFactor;
+
+
+	void Awake () {
+		
+		if (gControl == null) {
+			DontDestroyOnLoad (gameObject);
+			gControl = this;
+		} else if (gControl != this) {
+			Destroy (gameObject);
+		}
+	}
+
+
+
+	void Start()
+	{
+	//	LoadUserData ();
+	//	LoadQuestionData ();
+
+	}
+
+	//Saves de Usuário
+	public void SaveUserData()
+	{
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream file = File.Create (Application.dataPath + "/userData.dat");
+
+		UserData uData = new UserData ();
+		uData.pontos = pontos;
+		uData.funFactor = funFactor;
+
+		bf.Serialize (file, uData);
+		file.Close ();
+	}
+	public void LoadUserData()
+	{
+
+		if (File.Exists(Application.dataPath + "/userData.dat")) {
+
+			BinaryFormatter bf = new BinaryFormatter ();
+			FileStream file = File.Open (Application.dataPath + "/userData.dat",FileMode.Open);
+
+			UserData uData = (UserData)bf.Deserialize (file);
+			file.Close ();
+
+			pontos = uData.pontos;
+			funFactor = uData.funFactor;
+			Debug.Log ("LoadUserData OK");
+
+		}
+		else {
+			Debug.Log ("LoadUserData FAIL");
+			return;
+		}
+
+	}
+
+	//Saves de Banco de dados de Questões
+	public void SaveQuestionData()
+	{
+		BinaryFormatter bf = new BinaryFormatter ();
+		FileStream file = File.Create (Application.dataPath + "/questionData.dat");
+
+	//	QuestionData qData = new QuestionData ();
+	//	qData.perguntasBd = perguntasList;
+
+	//	bf.Serialize (file, qData);
+		file.Close ();
+	}
+	public void LoadQuestionData()
+	{
+		if (File.Exists(Application.dataPath + "/questionData.dat")){
+			BinaryFormatter bf = new BinaryFormatter ();
+			FileStream file = File.Open (Application.dataPath + "/questionData.dat",FileMode.Open);
+
+			QuestionData qData = (QuestionData)bf.Deserialize (file);
+			file.Close ();
+
+	//		perguntasList = qData.perguntasBd;
+			Debug.Log ("LoadQuestionData OK");
+		}
+		else {
+			Debug.Log ("LoadQestionData FAIL");
+		}
+
+	}
+
+}
+
+[Serializable]
+class UserData
+{
+	public int pontos;
+	public int funFactor;
+}
+
+[Serializable]
+class QuestionData
+{
+	public PerguntasClass[] perguntasBd;
+
+	public QuestionData(int TamanhoDaLista, List<PerguntasClass> Lista)
+	{
+		perguntasBd = new PerguntasClass[TamanhoDaLista];
+		for (int i = 0; i < TamanhoDaLista; i++) {
+			perguntasBd [i] = Lista [i];
+		}
+	}
+}
+
+class QuestionDataLoad
+{
+	public PerguntasClass[] perguntasBd;
+	public List<PerguntasClass> perguntasLista;
+}
+
+[Serializable]
+public class PerguntasClass : MonoBehaviour {
+
+	public string textoDaPerguntaBd;
+	public Sprite dica;
+	public AudioSource audioDaQuestao;
+	public List<RespostasClass> respostasBd = new List<RespostasClass>();
+	public int frequencia;
+	public int rating;
+	public enum Temas {Musica,Futebol,Radio,Arte,Outro};
+	public Temas tema; 
+
+
+	public PerguntasClass(string TextoDaPergunta,
+		Temas TemaDaQuestao,
+		string Resposta1Certa,
+		string Curiosidade1,
+		string Resposta2,
+		string Curiosidade2,
+		string Resposta3,
+		string Curiosidade3,
+		string Resposta4,
+		string Curiosidade4,
+		string Resposta5,
+		string Curiosidade5,
+		string Resposta6,
+		string Curiosidade6,
+		Sprite Dica = null,
+		AudioSource AudioDaQuestao = null)
+	{
+		textoDaPerguntaBd = TextoDaPergunta;
+		tema = TemaDaQuestao;
+		respostasBd.Add (new RespostasClass (RespostasClass.Estados.Normal,Resposta1Certa,Curiosidade1,true));
+		respostasBd.Add (new RespostasClass (RespostasClass.Estados.Normal,Resposta2,Curiosidade2));
+		respostasBd.Add (new RespostasClass (RespostasClass.Estados.Normal,Resposta3,Curiosidade3));
+		respostasBd.Add (new RespostasClass (RespostasClass.Estados.Normal,Resposta4,Curiosidade4));
+		respostasBd.Add (new RespostasClass (RespostasClass.Estados.Normal,Resposta5,Curiosidade5));
+		respostasBd.Add (new RespostasClass (RespostasClass.Estados.Normal,Resposta6,Curiosidade6));
+		audioDaQuestao = AudioDaQuestao;
+		dica = Dica;
+
+	}
+}
+[Serializable]
+public class RespostasClass : MonoBehaviour {
+
+	public Button clicavel;
+	public enum Estados{Normal,Tentado,Curioso,CuriosoPronto,CertoPronto,Certo};
+	public Estados estado;
+	public string textoDaResposta;
+	public string curiosidade;
+	public bool correta;
+
+
+	public RespostasClass(Estados Estado, string TextoDaResposta, string Curiosidade, Button botao = null)
+	{
+		estado = Estado;
+		textoDaResposta = TextoDaResposta;
+		curiosidade = Curiosidade;
+		clicavel = botao;
+	}
+	public RespostasClass(Estados Estado, string TextoDaResposta, string Curiosidade,bool Correta, Button botao = null)
+	{
+		estado = Estado;
+		textoDaResposta = TextoDaResposta;
+		curiosidade = Curiosidade;
+		correta = Correta;
+		clicavel = botao;
+	}
+}
+
+
---
+++ Assets/Scripts/ScriptsBanco/TesteCria1Questao.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class TesteCria1Questao : MonoBehaviour {
+
+	public GameObject pergunta;
+
+	// Use this for initialization
+	void Start () {
+		
+
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+
+	public void MostraBd()
+	{
+		for (int i = 0; i < GameControl.gControl.perguntasList.Count; i++) {
+			Debug.Log (GameControl.gControl.perguntasList [i].textoDaPerguntaBd);
+			for (int r = 0; r < GameControl.gControl.perguntasList [i].respostasBd.Count; r++) {
+				Debug.Log (GameControl.gControl.perguntasList [i].respostasBd [r].textoDaResposta);
+			}
+		}
+	}
+
+
+
+	public void printaJSON()
+	{
+		Debug.Log(GeraJSON ());
+	}
+
+	public string GeraJSON()
+	{
+		return JsonUtility.ToJson (GameControl.gControl.perguntasList [0]);
+	}
+
+	public void CriaQuestao()
+	{
+		GameControl.gControl.perguntasList.Add(
+			new PerguntasClass(
+				"Emerson Fittipaldi foi em 1972:",
+				PerguntasClass.Temas.Outro,
+				"Campeão mundial mais jovem de F1 até então.",
+				"Curiosidade1",
+				"Primeiro e único campeão brasileiro de F1 da história.",
+				"Curiosidade2",
+				"Campeão das 500 milhas de Indianápolis.",
+				"Curiosidade3",
+				"Que encerrou sua carreira após um acidente no michigan speedway.",
+				"Curiosidade4",
+				"Presenciou o primeiro campeão póstumo de F1.",
+				"Curiosidade5",
+				"Venceu a corrida de Inauguração do autódromo de interlagos.",
+				"Curiosidade6"));
+		Debug.Log(GameControl.gControl.perguntasList [0].respostasBd [2].textoDaResposta);
+		Debug.Log(GameControl.gControl.perguntasList [0].respostasBd [2].correta);
+
+
+	/*	pergunta = Instantiate (pergunta);
+		perguntaClass.textoDaPerguntaBd = "Emerson Fittipaldi foi em 1972:";
+		perguntaClass.respostasBd [0].textoDaResposta = "Primeiro e único campeão brasileiro de F1 da história.";
+		perguntaClass.respostasBd [0].correta = false;
+		perguntaClass.respostasBd [0].estado = RespostasClass.Estados.Normal;
+		perguntaClass.respostasBd [1].textoDaResposta = "Campeão mundial mais jovem de F1 até então.";
+		perguntaClass.respostasBd [1].correta = true;
+		perguntaClass.respostasBd [1].estado = RespostasClass.Estados.Normal;
+		perguntaClass.respostasBd [2].textoDaResposta = "Campeão das 500 milhas de Indianápolis.";
+		perguntaClass.respostasBd [2].correta = false;
+		perguntaClass.respostasBd [2].estado = RespostasClass.Estados.Normal;
+		perguntaClass.respostasBd [3].textoDaResposta = "Que encerrou sua carreira após um acidente no michigan speedway.";
+		perguntaClass.respostasBd [3].correta = false;
+		perguntaClass.respostasBd [3].estado = RespostasClass.Estados.Normal;
+		perguntaClass.respostasBd [4].textoDaResposta = "Presenciou o primeiro campeão póstumo de F1.";
+		perguntaClass.respostasBd [4].correta = false;
+		perguntaClass.respostasBd [4].estado = RespostasClass.Estados.Normal;
+		perguntaClass.respostasBd [5].textoDaResposta = "Venceu a corrida de Inauguração do autódromo de interlagos.";
+		perguntaClass.respostasBd [5].correta = false;
+		perguntaClass.respostasBd [5].estado = RespostasClass.Estados.Normal;
+
+		GameControl.gControl.perguntasBd [0]= pergunta;*/
+	}
+}
