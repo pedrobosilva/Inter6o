75c912a6d58c33ea4a2805ff8110e17892979442
ferraribrubs <bferrari96@gmail.com>
2016-11-14 22:57:14 -0200

    TweenCuriosidade
    colocado Tweens da curiosidade e pergunta

--- Assets/ButtonScript.cs
+++ Assets/ButtonScript.cs
@@ -7,6 +7,8 @@ public class ButtonScript : MonoBehaviour {
 
 	public static ButtonScript Instance;
 
+	public bool Ctrl_Verific = false;
+
 	public bool isCorrect = false;
 	public Text AnswerText;
 	public Text QuestionText;
@@ -45,6 +47,7 @@ public class ButtonScript : MonoBehaviour {
 
 	void Start()
 	{
+
 
 		posicaoInitial = new Vector3 (transform.position.x, transform.position.y, -7);
 
@@ -99,13 +102,20 @@ public class ButtonScript : MonoBehaviour {
 		if (tipo == type.resposta && gesture.ScreenPosition.y > gesture.PreviousScreenPosition.y) {
 			transform.position = new Vector3(transform.position.x, transform.position.y, -7.1f);
 			Debug.Log ("Colocar o iTween aqui");
-			iTween.MoveTo (gameObject, iTween.Hash (
-				"y", 1.5f,
-				"speed", 0.5f,
-				"easetype", iTween.EaseType.easeOutQuint));
 
-			/*iTween.RotateBy (gameObject, iTween.Hash (
-				"z", 20));*/
+			iTween.MoveTo (gameObject.transform.parent.gameObject, iTween.Hash (
+				"y", 1,
+				"speed", 0.5f,
+				"easetype", iTween.EaseType.easeOutQuint
+				));	
+
+			//"oncomplete", Ctrl_Verific = true
+
+			iTween.RotateBy (gameObject.transform.parent.gameObject, iTween.Hash (
+				"z", 10
+				));
+
+
 
 			// Não vai precisar do GoFlick = true, tira a linha que depois eu arrumo certinho.
 			//goFlick = true;
@@ -149,6 +159,9 @@ public class ButtonScript : MonoBehaviour {
 
 	void Update ()
 	{
+		
+
+
 		if (goFlick == true) {
 
 
